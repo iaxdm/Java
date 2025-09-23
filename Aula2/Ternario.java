614c9464e8b95449aca156f3ba07885c14cb36a6
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2025-09-23 20:48:33 -0300

    subir arquivos

---
+++ Aula2/Ternario.java
@@ -0,0 +1,6 @@
+public class Ternario {
+    public static void main(String[] args) {
+        int QntDiaMes = 28;
+        System.out.println(QntDiaMes < 30 ? "Mês é fevereiro" : "Mês não é fevereiro");
+    }
+}
