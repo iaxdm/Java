60620e6d75bf4217a18075211c833fc54caabce1
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2025-09-23 20:18:09 -0300

    subir arquivo

---
+++ Aula2/Maior.java
@@ -0,0 +1,11 @@
+public class Maior {
+    public static void main(String[] args) {
+        int valor1 = 15;
+        int valor2 = 30;
+            if (valor1 > valor2){
+                System.out.println("O número "+valor1+" é maior que o número "+valor2);
+            } else {
+                System.out.println("O número "+valor2+" é maior que o número "+valor1);
+            }
+    }
+}
