8770e187d4a47de1c83bdbb67f70a3cba62b3179
iaxdm <igormoraesadm@gmail.com>
2025-10-08 15:51:10 -0300

    atualizar

--- Aula7/SelectionSort/Ordenador.java
+++ Aula7/SelectionSort/Ordenador.java
@@ -1,20 +1,20 @@
 package Aula7.SelectionSort;
 
 public class Ordenador {
-    public static void bubbleSort(int[] vetor){
-        public static void selectionSort(int[] vetor){
-            int n = vetor.length;
-            for(int i = 0; i<n-1; i++){
-                System.out.printf("Posição i: %d \n", i);
-                int indiceDoMenor = i;
-                for(int j = i+1; j<n; j++){
-                    if(vetor[j] < vetor[indiceDoMenor]){
-                        System.out.printf("O número selecionado (j) é o %d - %d o número no indideDoMenor é %d - %d\n", j, vetor[j],
-                        indiceDoMenor, vetor[indiceDoMenor]);
-                        indiceDoMenor = j;
-                    }
+    public static void selectionSort(int[] vetor){
+        int n = vetor.length;
+        for(int i = 0; i<n-1; i++){
+            System.out.printf("Posição i: %d \n", i);
+            int indiceDoMenor = i;
+            for(int j = i+1; j<n; j++){
+                if(vetor[j] < vetor[indiceDoMenor]){
+                    System.out.printf("O número selecionado (j) é o %d - %d o número no indideDoMenor é %d - %d\n", j, vetor[j],
+                    indiceDoMenor, vetor[indiceDoMenor]);
+                    indiceDoMenor = j;
                 }
             }
+            int temp = vetor[indiceDoMenor];
+            vetor [i] = temp;
         }
     }
 }
