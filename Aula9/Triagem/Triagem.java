77c2a9d08019ffb634336080bc81645e72ec4f5c
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2025-10-28 18:20:59 -0300

    filas e pilhas

--- Aula9/Triagem/Triagem.java
+++ Aula9/Triagem/Triagem.java
@@ -1,29 +1,41 @@
+/*
+1. Triagem de Vacinação (Duas Filas) Desenvolva um programa que simule a triagem de uma fila de vacinação. Primeiro, crie uma Fila principal e adicione 5 objetos
+Pessoa com idades variadas (ex: 25, 65, 40, 70, 30). Em seguida, crie duas novas filas: FilaPrioridade e FilaComum. Processe a fila principal removendo uma pessoa
+de cada vez: se a idade da Pessoa for 60 anos ou mais, ela deve ser adicionada à FilaPrioridade; caso contrário, deve ir para a FilaComum. Ao final, imprima o estado
+das duas filas de destino.
+ */
+
 package Aula9.Triagem;
 import Aula9.Pessoa;
 import Aula9.FilaCircular.FilaCircular;
 
-import java.util.Arrays;
-
-public class Triagem {
+ public class Triagem {
     public static void main(String[] args) {
-        
-    System.out.println("Organizando Filas");
-    System.out.println("=================");
-    Pessoa p1 = new Pessoa("Carlos", 58);
-    Pessoa p2 = new Pessoa("Ana", 60);
-    Pessoa p3 = new Pessoa("Diego", 40);
-    Pessoa p4 = new Pessoa("Maria", 69);
-    Pessoa p5 = new Pessoa("Jorge", 34);
-    FilaCircular filaInicial = new FilaCircular(5);
-    FilaCircular filaPreferencial = new FilaCircular(5);
-    FilaCircular filaNormal = new FilaCircular(5);
+        int posicoes = 5;
+        FilaCircular filaDeEntrada = new FilaCircular(posicoes);
+        FilaCircular filaPrioridade = new FilaCircular(posicoes);
+        FilaCircular filaComum = new FilaCircular(posicoes);
 
-    filaInicial.enqueue(p1);
-    filaInicial.enqueue(p2);
-    filaInicial.enqueue(p3);
-    filaInicial.enqueue(p4);
-    filaInicial.enqueue(p5);
-    
-   
+        filaDeEntrada.enqueue(new Pessoa("Luis", 28));
+        filaDeEntrada.enqueue(new Pessoa("David", 27));
+        filaDeEntrada.enqueue(new Pessoa("Marco", 65));
+        filaDeEntrada.enqueue(new Pessoa("José", 70));
+        filaDeEntrada.enqueue(new Pessoa("Pedro", 20));
+
+        int tamanho = filaDeEntrada.tamanho();
+
+        for (int i = 0; i < tamanho; i++){
+            Pessoa p = filaDeEntrada.dequeue();
+            if(p.getIdade() >= 60){
+                filaPrioridade.enqueue(p);
+            }else{
+                filaComum.enqueue(p);
+            }
+        }
+
+        System.out.println("As pessoas na fila preferencial são: ");
+        filaPrioridade.MostrarFila();
+        System.out.println("As pessoas na fila comum são: ");
+        filaComum.MostrarFila();
     }
 }
